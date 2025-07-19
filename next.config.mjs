2836e81a6e34578946bb77623b456a09ba98b061
jose <mwalwamaj@gmail.com>
2025-07-20 01:35:40 +0300

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // This enables static export
+    // output: 'export', // This enables static export
 
   eslint: {
     ignoreDuringBuilds: true,
